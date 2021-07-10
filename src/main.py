79619fb62fbd5088b5e471174d9d10fd6095256e
inzenjer-banderas <lazar.neg@hotmail.com>
2021-07-10 15:23:38 +0200

    Added mechanism for determining day/night

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import os
+import suntimes
+import datetime
 
 
 class App:
@@ -8,9 +10,14 @@ class App:
         self.config_dict = {}
         with open(self.komorebi_config_file_path, 'r') as file:
             self.config_text = file.read().strip()
-
         self.convert_text_to_dict()
-        self.modify_config(wallpaper_name='proba_betmen')
+
+        self.latitude = 43.32472
+        self.longitude = 21.90333
+        self.altitude = 0  # Not so important
+        self.suntimes = suntimes.SunTimes(longitude=self.longitude, latitude=self.latitude, altitude=self.altitude)
+        self.wallpaper = self.determine_wallpaper_from_time()
+        self.modify_config(wallpaper_name=self.wallpaper)
 
     def modify_config(self, wallpaper_name='foggy_sunny_mountain'):
         self.config_dict['WallpaperName'] = wallpaper_name
@@ -24,6 +31,22 @@ class App:
 
     def convert_dict_to_text(self):
         return '\n'.join(['[KomorebiProperties]'] + ['='.join(pair) for pair in self.config_dict.items()])
+
+    def determine_wallpaper_from_time(self):
+        date_time = datetime.datetime.now()  # returns local date/time
+        rise_time = self.suntimes.riselocal(date_time)
+        set_time = self.suntimes.setlocal(date_time)
+
+        date_time = date_time.replace(tzinfo=rise_time.tzinfo)
+
+        if rise_time < date_time < set_time:
+            # Day
+            print('Day')
+            return 'proba_vitez'
+
+        # Night
+        print('Night')
+        return 'proba_betmen'
 
 
 if __name__ == '__main__':
